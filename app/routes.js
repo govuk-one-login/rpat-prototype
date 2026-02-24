34114eb41df02ccee09fbd08d6e5ac9e3b311aac
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-02-24 13:52:50 +0000

    ATO-2288: Move routes setup into journey folder

--- app/routes.js
+++ app/routes.js
@@ -3,58 +3,10 @@
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+const govukPrototypeKit = require("govuk-prototype-kit");
+const router = govukPrototypeKit.requests.setupRouter();
+const {
+  setupRoutes,
+} = require("./views/create-service-redirect-url-table/setup-routes");
 
-// Add your routes here
-router.get("/create-service-redirect-url-table/redirect-urls", (req, res) => {
-  res.render("/create-service-redirect-url-table/redirect-urls.html", {
-    redirectUrls: req.cookies.redirectUrls,
-    redirectUrlTableEntries: req.cookies.redirectUrlTableEntries,
-  });
-});
-
-router.post("/create-service-redirect-url-table/redirect-urls", (req, res) => {
-  const existingUrls = req.cookies.redirectUrls || [];
-  let allUrls = existingUrls;
-  if (req.body.redirectUrl) {
-    allUrls.push(req.body.redirectUrl);
-  }
-
-  res.cookie("redirectUrls", allUrls);
-  res.cookie("redirectUrlTableEntries", createTableEntries(allUrls));
-  res.redirect("/create-service-redirect-url-table/redirect-urls");
-});
-
-router.get(
-  "/create-service-redirect-url-table/redirect-urls/delete/:id",
-  (req, res) => {
-    let allUrls = req.cookies.redirectUrls;
-    allUrls.splice(req.params.id, 1);
-    res.cookie("redirectUrls", allUrls);
-    res.cookie("redirectUrlTableEntries", createTableEntries(allUrls));
-    res.redirect("/create-service-redirect-url-table/redirect-urls");
-  }
-);
-
-router.get("/create-service-redirect-url-table/confirm", (req, res) => {
-  const redirectUrls = req.cookies.redirectUrls;
-  res.render("/create-service-redirect-url-table/confirm.html", {
-    redirectUrls: redirectUrls,
-  });
-});
-const createTableEntries = (allUrls) => {
-  return allUrls.map((url, index) => {
-    return [
-      {
-        html: url.toString(),
-      },
-      {
-        html:
-          '<a href="/create-service-redirect-url-table/redirect-urls/delete/' +
-          index +
-          '">Delete</a>',
-      },
-    ];
-  });
-};
+setupRoutes(router);
