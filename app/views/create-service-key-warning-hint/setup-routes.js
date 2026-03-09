6549e88824859035abc5ac846f4e8b2d34ecbf3d
Louisa Nash <louisa.nash@softwire.com>
2026-03-09 10:11:52 +0000

    BAU: update key warning flows to use new redirect url table

---
+++ app/views/create-service-key-warning-hint/setup-routes.js
@@ -0,0 +1,62 @@
+function setupKeyWarningHintRoutes(router) {
+  // Add your routes here
+  router.get("/create-service-key-warning-hint/redirect-urls", (req, res) => {
+    res.render("/create-service-key-warning-hint/redirect-urls.html", {
+      redirectUrls: req.cookies.redirectUrls,
+      redirectUrlTableEntries: req.cookies.redirectUrlTableEntries,
+    });
+  });
+
+  router.post(
+    "/create-service-key-warning-hint/redirect-urls",
+    (req, res) => {
+      const existingUrls = req.cookies.redirectUrls || [];
+      let allUrls = existingUrls;
+
+      if (req.body.redirectUrl) {
+        allUrls.push(req.body.redirectUrl);
+      }
+
+      res.cookie("redirectUrls", allUrls);
+      res.cookie("redirectUrlTableEntries", createTableEntries(allUrls));
+      res.redirect("/create-service-key-warning-hint/redirect-urls");
+    }
+  );
+
+  router.get(
+    "/create-service-key-warning-hint/redirect-urls/delete/:id",
+    (req, res) => {
+      let allUrls = req.cookies.redirectUrls;
+      allUrls.splice(req.params.id, 1);
+      res.cookie("redirectUrls", allUrls);
+      res.cookie("redirectUrlTableEntries", createTableEntries(allUrls));
+      res.redirect("/create-service-key-warning-hint/redirect-urls");
+    }
+  );
+
+  router.get("/create-service-key-warning-hint/confirm", (req, res) => {
+    const redirectUrls = req.cookies.redirectUrls;
+    res.render("/create-service-key-warning-hint/confirm.html", {
+      redirectUrls: redirectUrls,
+    });
+  });
+  const createTableEntries = (allUrls) => {
+    return allUrls.map((url, index) => {
+      return [
+        {
+          html: url.toString(),
+        },
+        {
+          html:
+            '<a href="/create-service-key-warning-hint/redirect-urls/delete/' +
+            index +
+            '">Delete</a>',
+        },
+      ];
+    });
+  };
+}
+
+module.exports = {
+  setupKeyWarningHintRoutes,
+};
